c0271101b504d793e5d587195f6b1a157cb72559
Chris Kakos <ckakos@gmail.com>
2018-12-16 02:54:38 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,34 +1,34 @@
-# def consolidate_cart(cart)
-#     new_hash = {}
-#
-#     cart.each_with_index do |item|
-#         item.each do |food, value|
-#             if new_hash[food]
-#                 new_hash[food][:count] += 1
-#             else
-#                 new_hash[food] = value
-#                 new_hash[food][:count] = 1
-#             end
-#         end
-#     end
-#     new_hash
-# end
+def consolidate_cart(cart)
+    new_hash = {}
 
-def consolidate_cart(item)
-    new_hash = Hash.new 0
-
-    item.each do |hash|
-        hash.each do |food, description|
-            if new_hash.has_key?(food) == false
-                new_hash[food] = description
+    cart.each_with_index do |item|
+        item.each do |food, value|
+            if new_hash[food]
+                new_hash[food][:count] += 1
+            else
+                new_hash[food] = value
                 new_hash[food][:count] = 1
-            elsif new_hash.has_key?(food)
-                new_hash[food][:count] +=1
             end
         end
     end
     new_hash
 end
+
+# def consolidate_cart(item)
+#     new_hash = Hash.new 0
+# 
+#     item.each do |hash|
+#         hash.each do |food, description|
+#             if new_hash.has_key?(food) == false
+#                 new_hash[food] = description
+#                 new_hash[food][:count] = 1
+#             elsif new_hash.has_key?(food)
+#                 new_hash[food][:count] +=1
+#             end
+#         end
+#     end
+#     new_hash
+# end
 
 def apply_coupons(cart, coupons)
     # code here
