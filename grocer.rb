76ff450d54202555b5b6bcce6eabed6bf3a47903
Chris Kakos <ckakos@gmail.com>
2018-12-16 03:06:46 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -30,6 +30,25 @@ end
 #     new_hash
 # end
 
+
+
+def apply_coupons(cart, coupons)
+    # code here
+    cart.keys.each do |item|
+        coupons.each do |coupon|
+            if item == coupon[:item] && cart[item][:count] >= coupon[:num]
+                cart[item][:count] -= coupon[:num]
+                if cart["#{coupon[:item]} W/COUPON"]
+                    cart["#{coupon[:item]} W/COUPON"][:count] += 1
+                else
+                    cart["#{coupon[:item]} W/COUPON"] = {:price => coupon[:cost], :clearance => cart[item][:clearance], :count => 1}
+                end
+            end
+        end
+    end
+    cart
+end
+
 # def apply_coupons(cart, coupons)
 #   # code here
 #   result = {}
@@ -48,23 +67,6 @@ end
 #     end
 #     result
 # end
-
-def apply_coupons(cart, coupons)
-  # code here
-    cart.keys.each do |item|
-    coupons.each do |coupon|
-      if item == coupon[:item] && cart[item][:count] >= coupon[:num]
-        cart[item][:count] -= coupon[:num]
-        if cart["#{coupon[:item]} W/COUPON"]
-          cart["#{coupon[:item]} W/COUPON"][:count] += 1
-        else
-          cart["#{coupon[:item]} W/COUPON"] = {:price => coupon[:cost], :clearance => cart[item][:clearance], :count => 1}
-        end
-      end
-    end
-  end
-  cart
-end
 
 def apply_clearance(cart)
     # code here
