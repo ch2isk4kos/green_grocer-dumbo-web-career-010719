9ec50765a0b08116bd392f1a9169516127d10909
Chris Kakos <ckakos@gmail.com>
2018-12-16 02:52:07 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,31 +1,35 @@
-def consolidate_cart(cart)
-    new_hash = {}
-
-    cart.each_with_index do |item|
-        item.each do |food, value|
-            if new_hash[food]
-                new_hash[food][:count] += 1
-            else
-                new_hash[food] = value
-                new_hash[food][:count] = 1
-            end
-        end
-    end
-    new_hash
-end
-
 # def consolidate_cart(cart)
-#   # code here
-#   new_hash = {}
-#   cart.each do |item|
-#     item.each do |food, item|
-#       item[:count] = 0
-#       item[:count] += 1
-#       new_hash[food] = item
+#     new_hash = {}
+# 
+#     cart.each_with_index do |item|
+#         item.each do |food, value|
+#             if new_hash[food]
+#                 new_hash[food][:count] += 1
+#             else
+#                 new_hash[food] = value
+#                 new_hash[food][:count] = 1
+#             end
+#         end
 #     end
-#   end
 #     new_hash
 # end
+
+def consolidate_cart(item)
+  final = Hash.new 0 
+  count = :count
+item.each do |hash|
+  hash.each do |food, description|
+  
+if final.has_key?(food) == false
+  final[food] = description
+  final[food][count] = 1
+elsif final.has_key?(food)
+final[food][:count] +=1
+end
+end
+end
+final
+end
 
 def apply_coupons(cart, coupons)
     # code here
