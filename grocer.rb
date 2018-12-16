aedb554e2ccc1dc49ef2f8b0a45822f97a7b1275
Chris Kakos <ckakos@gmail.com>
2018-12-16 02:45:27 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -12,7 +12,7 @@ def consolidate_cart(cart:[])
   new_hash = {}
   cart.each do |item|
     item.each do |food, item|
-      item[:count] = 0
+      item[:count] || = 0
       item[:count] += 1
       new_hash[food] = item
     end
