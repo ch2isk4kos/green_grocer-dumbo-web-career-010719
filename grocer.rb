9c2f318fecf9d28f1ed7ab631a3e1e03f653b863
Chris Kakos <ckakos@gmail.com>
2018-12-16 02:53:41 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -16,13 +16,13 @@
 
 def consolidate_cart(item)
     final = Hash.new 0
-    count = :count
+    # count = :count
 
     item.each do |hash|
         hash.each do |food, description|
             if final.has_key?(food) == false
                 final[food] = description
-                final[food][count] = 1
+                final[food][:count] = 1
             elsif final.has_key?(food)
                 final[food][:count] +=1
             end
