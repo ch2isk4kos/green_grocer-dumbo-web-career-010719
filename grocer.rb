1fc3c70cbec1b11c7f750c7a5427cdc0940b4205
Chris Kakos <ckakos@gmail.com>
2018-12-16 02:48:08 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -8,19 +8,19 @@
 # end
 
 def consolidate_cart(cart)
-  result = {}
-  # code here	  # code here
-  cart.each_with_index do |item, i|
-    item.each do |food, info|
-      if result[food]
-        result[food][:count] += 1
-      else
-        result[food] = info
-        result[food][:count] = 1
-      end
+    result = {}
+
+    cart.each_with_index do |item, i|
+        item.each do |food, info|
+            if result[food]
+                result[food][:count] += 1
+            else
+                result[food] = info
+                result[food][:count] = 1
+            end
+        end
     end
-  end
-  result
+    result
 end
 
 # def consolidate_cart(cart)
